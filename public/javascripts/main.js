3a23f41e6533f3e3e9cda425fb92b3fde03ac232
evycham <ygruzevich8484@gmail.com>
2026-03-14 22:44:39 +0100

    basic js for getting all users and stocks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,16 +4,38 @@
 const username = document.getElementById("user-name");
 const userBalance = document.getElementById("user-balance");
 
-const topPlayers = document.getElementById("top-players");
+const topPlayers = document.getElementById("top");
 const stocksItem = document.getElementById("stocks-item-list");
 
 const templateTop = document.getElementById("players-template");
 const templateStock = document.getElementById("stocks-template");
 
+let isUpdating = false;
 
 getUserInfo();
 getAllUser();
 getStocks();
+
+setInterval(() => {
+    if(isUpdating) {
+        return;
+    }
+    isUpdating = true;
+    update();
+}, 2_000);
+
+async function update() {
+    try{
+        await getStocks();
+        await getUserInfo();
+        await getAllUser();
+        isUpdating = false;
+    } catch(error){
+        console.log(error);
+    } finally {
+        isUpdating = false;
+    }
+}
 
 /**
 * Anzeige des Budgets und Namens des eingelogten Spielers
@@ -48,14 +70,16 @@ async function getAllUser(){
 
         const data = await response.json();
 
-        // sortieren absteigend
-        data.sort((a, b) => {
-            return a.sum - b.sum;
+        // altes löschen
+        const users = Array.from(document.getElementsByClassName("player-item"));
+        users.forEach(user => {
+            user.remove();
         });
 
-        // altes löschen
-        const users = document.getElementsByClassName("")
-
+        // sortieren absteigend
+        data.sort((a, b) => {
+            return b.sum - a.sum;
+        });
 
         // anzeigen
         for(let i = 0; i < data.length; i++){
@@ -88,8 +112,15 @@ async function getStocks(){
             throw new Error(response.statusText);
         }
 
+        // altes löschen
+        const stocks = Array.from(document.getElementsByClassName("stock-item"));
+        stocks.forEach(stock => {
+            stock.remove();
+        })
+
         const data = await response.json();
 
+        // anzeigen
         data.forEach(stock => {
             const clone = templateStock.content.cloneNode(true);
             const stockName = clone.querySelector(".stock-name");
